066fee792403789b12525385080ff8406ac195a1
monci <9440331+monci@user.noreply.gitee.com>
2024-03-11 02:53:31 -0800

    Site updated: 2024-03-11 02:53:31

--- js/foot.js
+++ js/foot.js
@@ -8,7 +8,7 @@ $(document).ready(function(e){
 
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
+$('footer.custom_text').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
     window.setTimeout("show_date_time()", 1000);
     BirthDay=new Date("3/11/2024 0:0:0");
     today=new Date();
