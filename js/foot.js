d793c72bcc0d8653383bfcc77ff1500dae0ca483
monci <9440331+monci@user.noreply.gitee.com>
2024-03-11 02:58:30 -0800

    Site updated: 2024-03-11 02:58:30

--- js/foot.js
+++ js/foot.js
@@ -1,11 +1,9 @@
 (document).ready(function(e){
-    $('.copyright').html('©2024 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By Gryphon');
-})
-​
-$(document).ready(function(e){
+    $('.copyright').html('©2024 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By (秋意寒)Gryphon');
     show_date_time();
 })
-​
+
+
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
 $('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
